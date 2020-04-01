7bf240f1b35f9140c69e127167bc9aa972f9ba58
Karim ali <karim.ali14199314@gmail.com>
2020-04-01 17:09:44 +0200

    init

---
+++ app/src/main/java/com/example/diethub/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.diethub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
