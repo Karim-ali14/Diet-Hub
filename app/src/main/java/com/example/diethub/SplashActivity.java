16e1af16823d17c6a4a8e51f4175868a1f64e072
Karim ali <karim.ali14199314@gmail.com>
2020-04-01 17:14:47 +0200

    init splashActivity withOut image

--- app/src/main/java/com/example/diethub/SplashActivity.java
+++ app/src/main/java/com/example/diethub/SplashActivity.java
@@ -11,5 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    //startActivity(new Intent(getApplicationContext(), Login_Activity.class));
+                    finish(); // finish(); because This Activity close After move To Next Activity And Next Activity will Be Launcher
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
