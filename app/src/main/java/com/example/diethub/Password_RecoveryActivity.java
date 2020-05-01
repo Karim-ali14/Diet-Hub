3b5244976f541aa9eaa0f1c34acef093b873105a
Karim ali <karim.ali14199314@gmail.com>
2020-05-02 01:36:01 +0200

    about,enterCode,PassRecovery make er+en lang

--- app/src/main/java/com/example/diethub/Password_RecoveryActivity.java
+++ app/src/main/java/com/example/diethub/Password_RecoveryActivity.java
@@ -2,11 +2,13 @@ package com.example.diethub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Password_RecoveryActivity extends AppCompatActivity {
-
+    Button SendCodeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,12 @@ public class Password_RecoveryActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility
                 (View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
                         View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
+        SendCodeButton = findViewById(R.id.Send_Code_Button);
+        SendCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Password_RecoveryActivity.this,Enter_CodeActivity.class));
+            }
+        });
     }
 }
